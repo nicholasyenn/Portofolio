35378e21d292f83a6427102c77731efc52d94cb5
Nicholas Yen <42669022+NicholasYen@users.noreply.github.com>
2021-05-26 11:49:04 +0700

    first commit

---
+++ app/src/main/java/com/example/instagramandrofix/Fragments/HomeFragment.kt
@@ -0,0 +1,108 @@
+package com.example.instagramandrofix.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instagramandrofix.Adapter.PostAdapter
+import com.example.instagramandrofix.Model.Post
+import com.example.instagramandrofix.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+    private var postAdapter:PostAdapter? = null
+    private var postList:MutableList<Post>?=null
+    private var followingList:MutableList<Post>?=null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        var recyclerView: RecyclerView? = null
+        recyclerView = view.findViewById(R.id.recycler_view_home)
+        val linearLayoutManager = LinearLayoutManager(context)
+        linearLayoutManager.reverseLayout = true
+        linearLayoutManager.stackFromEnd = true
+        recyclerView.layoutManager = linearLayoutManager
+
+        postList = ArrayList()
+        postAdapter = context?.let { PostAdapter(it, postList as ArrayList<Post>) }
+        recyclerView.adapter = postAdapter
+
+        checkFollowings()
+
+        return view
+    }
+
+    private fun checkFollowings() {
+        followingList = ArrayList()
+
+        val followingRef = FirebaseDatabase.getInstance().reference
+                .child("Follow").child(FirebaseAuth.getInstance().currentUser!!.uid)
+                .child("Following")
+
+        followingRef.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()){
+                    (followingList as ArrayList<String>).clear()
+
+                    for (snapshot in dataSnapshot.children){
+                        snapshot.key?.let { (followingList as ArrayList<String>).add(it) }
+                    }
+
+                    retrievePosts()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+    private fun retrievePosts() {
+        val postsRef = FirebaseDatabase.getInstance().reference.child("Posts")
+        postsRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                postList?.clear()
+
+                for (snapshot in dataSnapshot.children){
+                    val post = snapshot.getValue(Post::class.java)
+
+                    for (id in (followingList as ArrayList<String>)){
+                        if (post!!.getPublisher() == id){
+                            postList!!.add(post)
+                        }
+
+                        postAdapter!!.notifyDataSetChanged()
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+}
